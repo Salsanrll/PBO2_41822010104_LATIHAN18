94ea6d22c64885cebaeb7463bc90ba2b25b644a2
Salsanrll <146332102+Salsanrll@users.noreply.github.com>
2023-09-28 22:51:49 +0700

    Add files via upload

---
+++ PBO241822010104Latihan18.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo2.pkg41822010104.latihan18;
+/**
+ *
+ * @author SALSA NURUL LAELI
+ * Nama :Salsa Nurul Laeli
+ * NIM  :41822010104
+ */
+public class PBO241822010104Latihan18 {
+    public static void main(String[] args) {
+     System.out.println("Format Tugas PBO");
+     double mulai = 3.5;
+        double akhir = 35.0;
+        double kelipatan = 3.5;
+
+        while (mulai <= akhir) {
+            System.out.println(mulai);
+            mulai += kelipatan;
+        }
+        System.out.println("\nDeveloped by: Salsa Nurul Laeli");
+    }
+    }
+
